b0e316e1d5bdff36ca7246a062a7c42c15379a95
Trey H <smirkinbunny@gmail.com>
2025-07-30 16:30:11 -0700

    Update backend to match Render env variables

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
   },
 });
 
